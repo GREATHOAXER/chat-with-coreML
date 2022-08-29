92e25566a446894b44abc425e4cc4f65b3cf5e30
한형서 <2017029243@hanyang.ac.kr>
2022-08-30 00:54:59 +0900

    :tada: Create new xcode project
    Create new xcode project using SwiftUI

---
+++ MLChat/MLChat/MLChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLChatApp.swift
+//  MLChat
+//
+//  Created by Hyung Seo Han on 2022/08/30.
+//
+
+import SwiftUI
+
+@main
+struct MLChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
